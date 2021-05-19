16d6ced14a8ca5542a6ac8f57244f7e6842e9e60
Angel Mary Alex <44723357+Angel-cec3@users.noreply.github.com>
2021-05-19 14:05:00 +0530

    002_exercise-1

---
+++ patterns.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void){
+
+int type = get_int("Enter type of pattern(1 or 2)");
+int r = get_int("Number of rows");
+int n = r;
+ switch(type){
+case 1:
+	for(int row=1;row<=r;row++){
+		for(int col=r;col>=row;col--){
+			printf("#");
+		}
+	  printf("\n");
+	}
+	break;
+case 2:
+	for(int row=1;row<=r;row++){
+		for(int col=1;col<=r;col++){
+			if(col>n-1){
+			  printf("#");
+			}
+			else{
+			  printf(" ");
+			}
+		}
+		n--;
+		printf("\n");
+	}
+	break;
+default:
+	printf("Error");		
+}
+}
