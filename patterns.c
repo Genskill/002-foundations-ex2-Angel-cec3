97d1f6ddadeb57b35ac16f4149b1c35c758112b2
Angel Mary Alex <44723357+Angel-cec3@users.noreply.github.com>
2021-05-19 15:47:24 +0530

    update patterns.c

--- patterns.c
+++ patterns.c
@@ -3,8 +3,8 @@
 
 int main(void){
 
-int type = get_int("Enter type of pattern(1 or 2)");
-int r = get_int("Number of rows");
+int type = get_int("Enter type of pattern(1 or 2) ");
+int r = get_int("Number of rows ");
 int n = r;
  switch(type){
 case 1:
