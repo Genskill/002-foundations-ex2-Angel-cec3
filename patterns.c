08cfb844efa5891b4980a7419e8d2650d80cb75a
Angel Mary Alex <44723357+Angel-cec3@users.noreply.github.com>
2021-05-19 16:06:37 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -3,26 +3,26 @@
 
 int main(void){
 
-int type = get_int("Enter type of pattern(1 or 2) ");
-int r = get_int("Number of rows ");
+int type = get_int("Enter type of pattern(1 or 2)");
+int r = get_int("Number of rows");
 int n = r;
  switch(type){
 case 1:
-	for(int row=1;row<=r;row++){
-		for(int col=r;col>=row;col--){
+	for(int row=0;row<r;row++){
+		for(int col=r;col>row;col--){
 			printf("#");
 		}
-	  printf("\n");
+		printf("\n");
 	}
 	break;
 case 2:
 	for(int row=1;row<=r;row++){
 		for(int col=1;col<=r;col++){
 			if(col>n-1){
-			  printf("#");
+			printf("#");
 			}
 			else{
-			  printf(" ");
+			printf(" ");
 			}
 		}
 		n--;
